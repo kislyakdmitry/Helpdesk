b262b13aac00897db148f723a521d00b19df4c06
Zuevsky <alexey_zuevsky@mail.ru>
2022-02-03 15:13:57 +0300

    docs: added empty project with liquibase changelogs

---
+++ src/test/java/innowise/zuevsky/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package innowise.zuevsky.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
