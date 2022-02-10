07bb43ee48ad725dd429a3bfc72068d6c26626fb
Zuevsky <alexey_zuevsky@mail.ru>
2022-02-10 10:38:16 +0300

    files: update migrations (changed tables)

--- src/main/java/innowise/zuevsky/helpdesk/domain/Ticket.java
+++ src/main/java/innowise/zuevsky/helpdesk/domain/Ticket.java
@@ -3,7 +3,6 @@ package innowise.zuevsky.helpdesk.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,7 +14,6 @@ import javax.persistence.Id;
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
     private Long id;
 
     private String name;
