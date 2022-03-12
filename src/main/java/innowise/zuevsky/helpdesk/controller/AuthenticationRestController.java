ade0ef39ad13b1e5aa47b692357ba44a9b9659f3
Zuevsky <alexey_zuevsky@mail.ru>
2022-03-12 12:51:30 +0300

    docs: configured security token

---
+++ src/main/java/innowise/zuevsky/helpdesk/controller/AuthenticationRestController.java
@@ -0,0 +1,60 @@
+package innowise.zuevsky.helpdesk.controller;
+
+import innowise.zuevsky.helpdesk.domain.User;
+import innowise.zuevsky.helpdesk.dto.AuthenticationRequestDto;
+import innowise.zuevsky.helpdesk.repository.UsersRepository;
+import innowise.zuevsky.helpdesk.security.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationRestController {
+
+    private final AuthenticationManager authenticationManager;
+    private UsersRepository usersRepository;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public AuthenticationRestController(AuthenticationManager authenticationManager, UsersRepository usersRepository, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.usersRepository = usersRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<Object, Object>> authenticate(@RequestBody AuthenticationRequestDto request) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+            User user = usersRepository.findByEmail(request.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+            String token = jwtTokenProvider.createToken(request.getEmail(), user.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", request.getEmail());
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            Map<Object, Object> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Invalid email/password combination");
+            errorResponse.put("httpStatus", HttpStatus.FORBIDDEN);
+            return ResponseEntity.ok(errorResponse);
+        }
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
+    }
+}
---
+++ src/main/java/innowise/zuevsky/helpdesk/dto/AuthenticationRequestDto.java
@@ -0,0 +1,10 @@
+package innowise.zuevsky.helpdesk.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationRequestDto {
+
+    private String email;
+    private String password;
+}
