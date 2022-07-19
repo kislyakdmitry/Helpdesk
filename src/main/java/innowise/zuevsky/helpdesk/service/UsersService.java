dffc75b1d4e5b26290143e7bfdb559396c99b61a
SvetilnikST <svetilnik.stezhkina@gmail.com>
2022-07-19 12:38:01 +0300

    Fix

--- src/main/java/innowise/zuevsky/helpdesk/service/UsersService.java
+++ src/main/java/innowise/zuevsky/helpdesk/service/UsersService.java
@@ -6,21 +6,30 @@ import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
+import javax.ws.rs.core.Context;
 import java.security.Principal;
 import java.util.List;
 
 @Controller
 public class UsersService {
 
+    @Context
+    SecurityContext context;
+
     public CurrentUser getCurrentUser() {
         CurrentUser currentUser = CurrentUser.builder().build();
+
         KeycloakAuthenticationToken authentication = (KeycloakAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
         Principal principal = (Principal) authentication.getPrincipal();
         if (principal instanceof KeycloakPrincipal) {
+
+            @SuppressWarnings("unchecked")
             KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
+
             currentUser.setUserName(kPrincipal.getKeycloakSecurityContext().getToken().getPreferredUsername());
 
             List<GrantedAuthority> roles = (List<GrantedAuthority>) authentication.getAuthorities();
